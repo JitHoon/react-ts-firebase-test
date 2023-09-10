0f64a6632bd8cab0bc8775155b8002f4bc446038
JitHoon <imessi0336@gmail.com>
2023-09-11 00:56:57 +0900

    asd

--- src/modules/CreateDoc.tsx
+++ src/modules/CreateDoc.tsx
@@ -2,11 +2,7 @@ import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase-config";
 
 export default async function createTestDoc() {
-  try {
-    await addDoc(collection(db, "tests"), {
-      hello: "hello~",
-    });
-  } catch (err) {
-    console.log(`createTestDoc err: ${err}`);
-  }
+  await addDoc(collection(db, "tests"), {
+    hello: "hello~",
+  });
 }
